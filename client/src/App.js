ab17c297870642b3a3365da632e9b39c53d6fb92
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-07-21 08:59:46 -0700

    adding notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
+//imports useState to set slices of state and useEffect for sid eeffects 
 import axios from 'axios';
-import { Switch , Route } from 'react-router-dom';
+//imports library
+import { Route } from 'react-router-dom';
+//imports Route tell where to go,  use Switch if using  nav bar and switching pages
 import SavedList from './Movies/SavedList';
 import Movielist from './Movies/MovieList'
 import Movie from './Movies/Movie'
+//these are imports of childs of the App file importing them so they can be used
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -25,29 +29,26 @@ const App = () => {
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
-    let x = []
-    let y = movieList.find(movieItem=>{
+    // let x = []
+    let chosenMovie = movieList.find(movieItem=>{
       return movieItem.id == id
     })
-    x.push(y)
-    setSaved(y)
+    setSaved(chosenMovie)
   };
 
   return (
-    // add Switch instead of div?
     <div> 
       <SavedList list={[ saved ]} />
+      {/* this is the div at the top 'header' */}
 
-    <Switch>
       <Route path='/movies/:itemId'>
         <Movie movies={movieList} addToSavedList={addToSavedList}/>
       </Route>
 
-      <Route path='/'>
+      <Route exact path='/'>
         <Movielist movies={movieList}/>
             {/* **This component will need the movies injected into it via props**. */}
       </Route>
-    </Switch>
     </div>
   );
 };
